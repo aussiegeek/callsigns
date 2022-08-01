4e9ce304a40ee565c996698ceea4036a1b4caec0
Alan Harper <475+aussiegeek@users.noreply.github.com>
2022-08-01 20:50:15 +1000

    Move types to separate file

---
+++ au/types.ts
@@ -0,0 +1,10 @@
+export interface Callsign {
+  name: string;
+  state: string;
+  postcode: string;
+  callsign: string;
+  category: string;
+  issuedDate: string | null;
+  effectiveDate: string | null;
+  expiryDate: string | null;
+}
